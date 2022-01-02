a2c6873de7fc5ff2fc8e70b9cb85b0c2f3913b31
aya mohamed <96661510+Aya-M-Hammad@users.noreply.github.com>
2022-01-02 13:53:48 +0200

    Create linux-video.php

---
+++ linux-video.php
@@ -0,0 +1,58 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+
+
+<style type="text/css" media="all">
+   html { 
+   direction:rtl;
+
+          background: url(background.jpg) no-repeat center center fixed; 
+          -webkit-background-size: cover;
+          -moz-background-size: cover;
+          -o-background-size: cover;
+          background-size: cover;
+          }
+         
+          body {
+               
+              background: none;
+               
+          }
+           
+          .container {
+               
+              text-align: center;
+              margin-top: 100px;
+              width: 450px;
+               
+          }
+           
+          h1 {
+    font-size: 2.5rem;
+    font-family: cairo;
+}
+</style>
+  <title>Aya Hsoub Linux </title>
+</head>
+<body>
+    <div class="container">
+      
+          <h1>Aya Hsoub Linux</h1>
+          
+          <br>
+          
+      
+          <div id="video"></div>
+      </div>
+<div style="position: relative; padding-bottom: 56.25%; height: 0;"><iframe src="https://www.loom.com/embed/b7e8f6386b0d4ba6b4a0d3c8d2960c6e" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe></div>
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
+</html>
